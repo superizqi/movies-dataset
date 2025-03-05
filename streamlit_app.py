c9258d666cb0627edde716032dcde45faf6e07e0
Rizqi Prima <rizqiphd@gmail.com>
2025-03-05 16:27:36 +0700

    try

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,12 @@ import streamlit as st
 
 # conn = st.connection('pets_db', type='sql')
 
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM mytable;', ttl="10m")
+
 # Show the page title and description.
 st.set_page_config(page_title="Movies dataset", page_icon="🎬")
 st.title("🎬 Movies dataset")
@@ -46,13 +52,13 @@ st.write(
 
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
-@st.cache_data
-def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
-    return df
+# @st.cache_data
+# def load_data():
+#     df = pd.read_csv("data/movies_genres_summary.csv")
+#     return df
 
 
-df = load_data()
+# df = load_data()
 
 # Display the data as a table using `st.dataframe`.
 st.dataframe(
