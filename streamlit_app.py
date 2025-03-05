57347b4a9071358577cb8a564b22500bd4b7172e
Rizqi Prima <rizqiphd@gmail.com>
2025-03-05 16:06:45 +0700

    connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 import streamlit as st
 
-conn = st.experimental_connection('pets_db', type='sql')
+conn = st.connection('pets_db', type='sql')
 
 # Show the page title and description.
 st.set_page_config(page_title="Movies dataset", page_icon="🎬")
