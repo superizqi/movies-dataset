c1ff1d71885aeb9f78a127bb3f875f3677386050
Rizqi Prima <rizqiphd@gmail.com>
2025-03-05 15:31:25 +0700

    hello

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,7 @@ st.set_page_config(page_title="Movies dataset", page_icon="🎬")
 st.title("🎬 Movies dataset")
 st.write(
     """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
+    Hello World
     """
 )
 
